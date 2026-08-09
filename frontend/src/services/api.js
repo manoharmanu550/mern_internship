2ae6b18f604b0ab3c9d307aaf8db80ff0198d5a1
Manohar Mano <manoharmanu1618@gmail.com>
2026-08-09 16:14:06 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mern-internship-backend-p9ri.onrender.com",
+  baseURL: "https://mern-internship-backend-p9ri.onrender.com/api",
 });
 
 export default API;
